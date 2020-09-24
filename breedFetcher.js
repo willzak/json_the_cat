25563f63fbdac127ddc2480ca4a25f0fa14174ef
William <williamzak0@gmail.com>
2020-09-24 23:05:01 +0000

    refactored and tested func

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,28 @@
 const request = require('request');
-let breed = process.argv.slice(2)[0];
 
-const fetch = (breed) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?name=${breed}`, (error, status, body) => {
-    if (error) return console.log('!! Error Encountered: ', error);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?name=${breedName}`, (error, status, body) => {
+    let desc = '';
     let data = JSON.parse(body)[0];
-
-    if (data === undefined) {
-      console.log('🚫🐈 Breed not found: please input a correct breed name');
-    } else {
-      console.log(data['description']);
+    if (data !== undefined) {
+      desc = data['description'];
     }
+
+    callback(error, desc);
   });
 };
 
-fetch(breed);
+module.exports = { fetchBreedDescription };
+
+// const fetch = (breed) => {
+//   request(`https://api.thecatapi.com/v1/breeds/search?name=${breed}`, (error, status, body) => {
+//     if (error) return console.log('!! Error Encountered: ', error);
+//     let data = JSON.parse(body)[0];
+
+//     if (data === undefined) {
+//       console.log('🚫🐈 Breed not found: please input a correct breed name');
+//     } else {
+//       console.log(data['description']);
+//     }
+//   });
+// };
